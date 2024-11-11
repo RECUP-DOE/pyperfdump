5e8492377fa26f738d68ac54fe7891ab30a9b0bc
chaseleif <chaseleif@icloud.com>
2024-11-11 10:34:44 -0600

    advance v1.0, changed cmake for DECREF define

--- spack/repo/packages/py-perfdump/package.py
+++ spack/repo/packages/py-perfdump/package.py
@@ -13,7 +13,7 @@ class PyPerfdump(CMakePackage):
 
     version(
         "1.0",
-        sha256="d8777787a8f8b650fc1d6cd8cea23fd1a0c203c6654466c8bb9d407f59766b24",
+        sha256="",
     )
 
     variant("mpi", default=True, description="Use MPI")
