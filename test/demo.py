8e564666b3422a7874b01248dd2eeda1e5cb18db
chaseleif <chaseleif@icloud.com>
2025-02-25 14:11:20 -0600

    Simplify demo script

--- test/demo.py
+++ test/demo.py
@@ -1,18 +1,12 @@
 #! /usr/bin/env python3
 
-# The test can run and succeed both with and without MPI
+# This demo can run and succeed both with and without MPI+mpi4py
 try:
-  import mpi4py
-  # We could leave initialize and finalize as True here
-  # With these set to False, we must manually initialize() and finalize() MPI
-  mpi4py.rc.initialize = False
-  mpi4py.rc.finalize = False
-  # Otherwise, initialize and finalize would be handled just by importing MPI
   from mpi4py import MPI
-except Exception as e:
-  MPI = None
+except ModuleNotFoundError:
+  pass
 
-import pyperfdump, sys
+import pyperfdump
 
 # debugmsg can be swapped to optionally silence script messages
 #debugmsg = lambda *args, **kwargs: None
@@ -25,13 +19,6 @@ def run():
       i+=123
 
 if __name__=='__main__':
-  if MPI is not None:
-    debugmsg('Calling MPI.Init()',flush=True)
-    MPI.Init()
-    comm = MPI.COMM_WORLD
-    mpirank = comm.Get_rank()
-    mpisize = comm.Get_size()
-    debugmsg(f'{mpirank=}, {mpisize=}',flush=True)
   debugmsg('Calling pyperfdump.init()',flush=True)
   pyperfdump.init()
   debugmsg('Calling pyperfdump.start_region()',flush=True)
@@ -46,8 +33,4 @@ if __name__=='__main__':
   pyperfdump.end_region()
   debugmsg('Calling pyperfdump.finalize()',flush=True)
   pyperfdump.finalize()
-  if MPI is not None:
-    debugmsg('Calling MPI.Finalize()',flush=True)
-    MPI.Finalize()
-  sys.exit(0)
 
